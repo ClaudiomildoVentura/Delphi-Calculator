abbc0a505b9d4fde576b3ab4ef4ff0893ea50085
C.VENTURA <claudiomildo@gmail.com>
2020-02-29 15:29:41 -0300

    Starting development

---
+++ Calculator.pas
@@ -0,0 +1,24 @@
+unit Calculator;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TfrmCalculator = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmCalculator: TfrmCalculator;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ ProjectCalculator.dpr
@@ -0,0 +1,14 @@
+program ProjectCalculator;
+
+uses
+  Vcl.Forms,
+  Calculator in 'Calculator.pas' {frmCalculator};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmCalculator, frmCalculator);
+  Application.Run;
+end.
